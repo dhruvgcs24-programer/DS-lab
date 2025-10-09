42cb29e03c571d7db91e225969c3cf53bc2d226b
dhruvgcs24-programer <dhruvg.cs24@bmsce.ac.in>
2025-10-09 11:57:09 +0530

    Add files via upload

---
+++ infixtopostfix.c
@@ -0,0 +1,79 @@
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define SIZE 100
+
+char stack[SIZE];
+int top = -1;
+
+void push(char c) {
+    stack[++top] = c;
+}
+
+char pop() {
+    return stack[top--];
+}
+
+char peek() {
+    return stack[top];
+}
+
+int precedence(char op) {
+    if (op == '+' || op == '-') return 1;
+    if (op == '*' || op == '/') return 2;
+    return 0;
+}
+
+int isOperator(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/');
+}
+
+void infixToPostfix(char infix[], char postfix[]) {
+    int i = 0, j = 0;
+    char ch;
+
+    while (infix[i] != '\0') {
+        ch = infix[i];
+
+        if (isalnum(ch)) {
+            postfix[j++] = ch;
+        }
+        else if (ch == '(') {
+            push(ch);
+        }
+        else if (ch == ')') {
+            while (peek() != '(') {
+                postfix[j++] = pop();
+            }
+            pop();
+        }
+        else if (isOperator(ch)) {
+            while (top != -1 && precedence(peek()) >= precedence(ch)) {
+                postfix[j++] = pop();
+            }
+            push(ch);
+        }
+
+        i++;
+    }
+
+    while (top != -1) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+}
+
+int main() {
+    char infix[SIZE], postfix[SIZE];
+
+    printf("Enter infix expression: ");
+    scanf("%s", infix);
+
+    infixToPostfix(infix, postfix);
+
+    printf("Postfix expression: %s\n", postfix);
+
+    return 0;
+}
