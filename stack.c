0e369e3e77910aa9f005bb4cafdfb7517c6be2c1
dhruvgcs24-programer <dhruvg.cs24@bmsce.ac.in>
2025-10-09 11:49:14 +0530

    Add files via upload

---
+++ stack.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#define MAX 100
+
+int stack[MAX];
+int top = -1;
+
+// Push function
+void push(int value) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow! Cannot push %d\n", value);
+    } else {
+        top++;
+        stack[top] = value;
+        printf("%d pushed to stack\n", value);
+    }
+}
+
+// Pop function
+int pop() {
+    if (top == -1) {
+        printf("Stack Underflow! No element to pop.\n");
+        return -1;
+    } else {
+        int popped = stack[top];
+        top--;
+        return popped;
+    }
+}
+
+int main() {
+    int choice, value;
+
+    while(1) {
+        printf("\nStack Operations Menu:\n");
+        printf("1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1:
+                printf("Enter value to push: ");
+                scanf("%d", &value);
+                push(value);
+                break;
+
+            case 2:
+                value = pop();
+                if (value != -1) {
+                    printf("Popped element: %d\n", value);
+                }
+                break;
+
+            case 3:
+                printf("Exiting program.\n");
+                return 0;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+
+    return 0;
+}
